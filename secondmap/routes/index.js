0b756dab7997b0038205c1a3e758ccc4548e8330
a <a@aui-MacBookAir.local>
2021-09-16 15:44:33 +0900

    upload page - router upload page added

--- secondmap/routes/index.js
+++ secondmap/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/upload', (req, res, next)=> {
+  res.render('upload')
+});
+
 module.exports = router;
