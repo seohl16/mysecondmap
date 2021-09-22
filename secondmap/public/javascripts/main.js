31c0c68fe52eedf34d26ba0fec4c10bb29abba5a
selim <selim@student.42seoul.kr>
2021-09-22 18:26:46 +0900

    지도 위에 저장한 데이터 표시하기

--- secondmap/public/javascripts/main.js
+++ secondmap/public/javascripts/main.js
@@ -5,75 +5,66 @@ var mapOptions = {
 
 var map = new naver.maps.Map('map', mapOptions);
 
-const data = [
-	{
-		category_name: "서비스,산업 > 인터넷,IT > 게임업체",
-		place_name: "넷마블컴퍼니",
-		place_url: "http://place.map.kakao.com/26426436",
-		road_address_name: "서울 구로구 디지털로26길 38",
-		lat: 37.479849234077534,
-		lng: 126.89532072025125,
-	},
-	{
-		category_name: "서비스,산업 > 인터넷,IT > 게임업체",
-		place_name: "크래프톤",
-		place_url: "http://place.map.kakao.com/1818548732",
-		road_address_name: "서울 강남구 테헤란로 231",
-		lat: 37.50288207510758,
-		lng: 127.04135620138392, 
-	}
-];
-let markerList = [];
-let infowindowList = [];
+$.ajax({
+	url:"/location",
+	type:"GET",
+}).done((response) => {
+	if (response.message !== "success") return ;
+	const data = response.data;
 
-const getClickHandler = (i) => () => {
-	const marker = markerList[i];
-	const infowindow = infowindowList[i];
-	if (infowindow.getMap()){
+	let markerList = [];
+	let infowindowList = [];
+
+	const getClickHandler = (i) => () => {
+		const marker = markerList[i];
+		const infowindow = infowindowList[i];
+		if (infowindow.getMap()){
+			infowindow.close();
+		} else {
+			infowindow.open(map, marker);
+		}
+	};
+	// function getClickHandler(i) { return function () {}}
+
+	const getClickMap = (i) => () => {
+		const infowindow = infowindowList[i];
 		infowindow.close();
-	} else {
-		infowindow.open(map, marker);
 	}
-};
-// function getClickHandler(i) { return function () {}}
 
-const getClickMap = (i) => () => {
-	const infowindow = infowindowList[i];
-	infowindow.close();
-}
+	for (let i in data){
+		const target = data[i];
+		const latlng = new naver.maps.LatLng(target.lat, target.lng);
 
-for (let i in data){
-	const target = data[i];
-	const latlng = new naver.maps.LatLng(target.lat, target.lng);
+		let marker = new naver.maps.Marker({
+			map:map, 
+			position : latlng,
+			icon : {
+				content : `<div class='marker'></div>`, 
+				archor : new naver.maps.Point(7.5, 7.5), 
+			},
+		});
 
-	let marker = new naver.maps.Marker({
-		map:map, 
-		position : latlng,
-		icon : {
-			content : `<div class='marker'></div>`, 
-			archor : new naver.maps.Point(7.5, 7.5), 
-		},
-	});
+		const content = `
+			<div class="infowindow_wrap">
+				<div class="infowindow_name">${target.title}</div>
+				<div class="infowindow_address">${target.address}</div>
+			</div>
+		`;
 
-	const content = `
-		<div class="infowindow_wrap">
-			<div class="infowindow_name">${target.place_name}</div>
-			<div class="infowindow_address">${target.road_address_name}</div>
-		</div>
-	`;
+		const infowindow = new naver.maps.InfoWindow({
+			content:content, 
+			backgroundColor : "#00ff0000", 
+			borderColor : "#00ff0000", 
+			anchorSize : new naver.maps.Size(0,0), 
+		});
 
-	const infowindow = new naver.maps.InfoWindow({
-		content:content, 
-		backgroundColor : "#00ff0000", 
-		borderColor : "#00ff0000", 
-		anchorSize : new naver.maps.Size(0,0), 
-	});
+		markerList.push(marker);
+		infowindowList.push(infowindow);
+	}
 
-	markerList.push(marker);
-	infowindowList.push(infowindow);
-}
+	for (let i = 0, ii = markerList.length; i < ii; i++){
+		naver.maps.Event.addListener(map, "click", getClickMap(i));
+		naver.maps.Event.addListener(markerList[i], "click", getClickHandler(i));
+	} 
 
-for (let i = 0, ii = markerList.length; i < ii; i++){
-	naver.maps.Event.addListener(map, "click", getClickMap(i));
-	naver.maps.Event.addListener(markerList[i], "click", getClickHandler(i));
-} 
+});
