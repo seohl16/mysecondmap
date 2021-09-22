5c7f1aee4781e5499335d2a80c67dffe008845a7
selim <selim@student.42seoul.kr>
2021-09-22 16:04:13 +0900

    키워드 검색 데이터 받아오기 #keyword placeSearchCB

--- secondmap/public/javascripts/upload.js
+++ secondmap/public/javascripts/upload.js
@@ -1,7 +1,34 @@
- const mapContainer = document.getElementById("map");
- const mapOption = {
-	 center: new daum.maps.LatLng(37.554477, 126.970419),
-	 level:3,
- };
+const mapContainer = document.getElementById("map");
+const mapOption = {
+	center: new daum.maps.LatLng(37.554477, 126.970419),
+	level:3,
+};
 
- let map= new daum.maps.Map(mapContainer, mapOption);
+let map= new daum.maps.Map(mapContainer, mapOption);
+
+let infowindow = new daum.maps.InfoWindow({
+	zIndex : 1,
+});
+
+let markerList = [];
+
+let ps = new daum.maps.services.Places();
+
+searchPlaces();
+
+function searchPlaces() {
+	let keyword = $("#keyword").val();
+	ps.keywordSearch(keyword, placesSearchCB);
+};
+
+function placesSearchCB(data, status, pagination) {
+	if (status === daum.maps.services.Status.OK) {
+		console.log(data);
+	} else if (status === daum.maps.services.Status.ZERO_RESULT) {
+		alert("검색 결과가 존재하지 않습니다.");
+		return ;
+	} else if (status === daum.maps.services.Status.ERROR) {
+		alert("오류가 발생했습니다.");
+		return ;
+	};
+}
